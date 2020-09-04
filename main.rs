1b68dffa39730c61eb06ca39f0a03a02d30dd02b
Nikos Filippakis <me@nfil.dev>
2020-09-04 19:11:25 +0200

    Gen a simple img
    Signed-off-by: Nikos Filippakis <me@nfil.dev>

--- main.rs
+++ main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn write_to_file() -> std::io::Result<()> {
+    let mut f = File::create("out.ppm")?;
+    let width = 256;
+    let height = 256;
+    let head: String = format!("P3\n{} {}\n255\n", width, height);
+    f.write_all(head.as_bytes())?;
+
+    for (cnt, y) in (0..height).rev().enumerate() {
+        eprint!("\rLine {} of {}", cnt+1, height);
+        for x in 0..width {
+            let r = (x as f64) / (width as f64 - 1.);
+            let g = (y as f64) / (height as f64 - 1.);
+            let b = 0.25f64;
+
+            let px: String = format!("{} {} {}\n", (r*255.999) as u8, (g*255.999) as u8, (b*255.999) as u8);
+            f.write_all(px.as_bytes())?;
+        }
+    }
+    eprint!("\n");
+
+    Ok(())
+}
+
 fn main() {
+    write_to_file().unwrap();
     println!("Hello")
 }
